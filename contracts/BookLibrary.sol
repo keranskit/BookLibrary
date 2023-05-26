be2d71b9261a15c8fdb5562d55c8ad86c3bd15dc
Trayan Keranov <keranskit@gmail.com>
2023-05-26 14:43:29 +0300

    nt

--- contracts/BookLibrary.sol
+++ contracts/BookLibrary.sol
@@ -12,7 +12,7 @@ contract BookLibrary is Ownable {
 
     bytes32[] public bookKey;
 
-    mapping(bytes32 => Book) public books;
+    mapping (bytes32 => Book) public books;
     mapping (address => mapping(bytes32 => bool)) public borrowedBook;
 
     event LogAddedBook(string title, uint copies);
@@ -31,10 +31,12 @@ contract BookLibrary is Ownable {
     }
 
     function addBook(string memory _title, uint8 _copies) public onlyOwner validBookData(_title, _copies) bookDoesNotExist(_title) {
+        bytes32 bookNameEncoded = keccak256(abi.encodePacked(_title));
         address[] memory borrowed;
         Book memory newBook = Book(_copies, _title, borrowed);
-        books[keccak256(abi.encodePacked(_title))] = newBook;
-        bookKey.push(keccak256(abi.encodePacked(_title)));
+        books[bookNameEncoded] = newBook;
+        bookKey.push(bookNameEncoded);
+
         emit LogAddedBook(_title, _copies);
     }
 
@@ -65,5 +67,26 @@ contract BookLibrary is Ownable {
     function getAllAddressesBorrowedBook(bytes32 bookId) public view returns(address[] memory _book) {
         Book memory book = books[bookId];
         return book.bookBorrowedAddresses;
+    }
+
+    function getAvailableBooks() public view returns(Book[] memory) {
+        uint8 counter = 0;
+        for (uint i = 0; i < bookKey.length ; i++) {
+            bytes32 currentBookKey = bookKey[i];
+            Book memory currentBook = books[currentBookKey];
+            if (currentBook.copies > 0) {
+                counter++;
+            }
+        }
+
+        Book[] memory availableBooks = new Book[](counter);
+
+        for (uint i; i < bookKey.length; i++) {
+            if (books[bookKey[i]].copies > 0) {
+                availableBooks[i] = books[bookKey[i]];
+            }
+        }
+
+        return availableBooks;
     }
 }
